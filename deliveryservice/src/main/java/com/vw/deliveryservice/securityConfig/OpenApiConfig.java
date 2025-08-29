0a8a6ccc2789041bcf304b35c1bd884ebbbc9503
Ankush Jadon <ankushjadon258@gmail.com>
2025-08-29 14:30:55 +0900

    Updated code with swagger

---
+++ deliveryservice/src/main/java/com/vw/deliveryservice/securityConfig/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.vw.deliveryservice.securityConfig;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Delivery Service API")
+                        .version("1.0")
+                        .description("API documentation for Delivery Service"))
+                        .addSecurityItem(new SecurityRequirement().addList("JavaInUseSecurityScheme"))
+                        .components(new Components().addSecuritySchemes("JavaInUseSecurityScheme", new SecurityScheme()
+                        .name("JavaInUseSecurityScheme").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+
+    }
+}
